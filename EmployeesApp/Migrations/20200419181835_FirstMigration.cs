dfda6887cb0bb7465b03b735590490ab58e6c5a0
Helena Coppieters <helenacoppieters@yahoo.com>
2020-04-19 20:28:05 +0200

    Add project files.

---
+++ EmployeesApp/Migrations/20200419181835_FirstMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeesApp.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    AccountNumber = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "Id", "AccountNumber", "Age", "Name" },
+                values: new object[] { new Guid("d36f3fa4-5d03-43d2-98d8-ff7a1dc3a296"), "123-3452134543-32", 30, "Mark" });
+
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "Id", "AccountNumber", "Age", "Name" },
+                values: new object[] { new Guid("f3d3ba87-3d7c-49ed-99df-1469a5cd0284"), "123-9384613085-55", 28, "Evelin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
